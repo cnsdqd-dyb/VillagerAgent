84778bec77d6c70a0f7b4bf43aa3b5fac9ff23d9
Zhengzhe Pan <85882612@qq.com>
2025-01-14 16:42:53 +0800

    use item on block discription adjusted

--- start_with_config.py
+++ start_with_config.py
@@ -54,7 +54,7 @@ def run(api_model: str, api_base: str, task_type: str, task_idx: int, agent_num:
         agent_tool = [Agent.placeBlock, Agent.fetchContainerContents, Agent.MineBlock, Agent.scanNearbyEntities, Agent.equipItem,
                       Agent.navigateTo, Agent.withdrawItem, Agent.ToggleAction, Agent.handoverBlock]
     elif task_type == "meta":
-        agent_tool = [Agent.scanNearbyEntities, Agent.navigateTo, Agent.attackTarget, Agent.useItemOnEntity, 
+        agent_tool = [Agent.scanNearbyEntities, Agent.navigateTo, Agent.attackTarget, Agent.useItemOnEntity, Agent.useItemOnBlock,
                       Agent.MineBlock, Agent.placeBlock, Agent.equipItem, Agent.handoverBlock, Agent.SmeltingCooking, Agent.withdrawItem, 
                       Agent.storeItem, Agent.craftBlock, Agent.eat, Agent.fetchContainerContents, Agent.wake, Agent.talkTo, Agent.waitForFeedback,
                       Agent.openContainer, Agent.performMovement, 
@@ -170,7 +170,7 @@ def run(api_model: str, api_base: str, task_type: str, task_idx: int, agent_num:
 if __name__ == "__main__":
     # with open("meta_test_config.json", "r") as f:
     # with open("/home/yubo/VillagerAgent-Minecraft-multiagent-framework/test_config.json", "r") as f:
-    with open("deepseek_chat_launch_config_meta.json", "r") as f:
+    with open("meta_test_config.json", "r") as f:
         launch_config = json.load(f)
     # shuffle 
     # launch_config = random.sample(launch_config, len(launch_config))
