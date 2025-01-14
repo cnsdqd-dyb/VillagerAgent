8f1b848234087ddb8aba023acee478dac1fa2007
Zhengzhe Pan <85882612@qq.com>
2025-01-14 16:43:10 +0800

    use item on block discription adjusted

--- start_with_config.py
+++ start_with_config.py
@@ -170,7 +170,7 @@ def run(api_model: str, api_base: str, task_type: str, task_idx: int, agent_num:
 if __name__ == "__main__":
     # with open("meta_test_config.json", "r") as f:
     # with open("/home/yubo/VillagerAgent-Minecraft-multiagent-framework/test_config.json", "r") as f:
-    with open("meta_test_config.json", "r") as f:
+    with open("deepseek_chat_launch_config_meta.json", "r") as f:
         launch_config = json.load(f)
     # shuffle 
     # launch_config = random.sample(launch_config, len(launch_config))
